f78ab85d4d742ddf858c9e47ad9ba827971b0aac
Lilianna Guderska <lilianna.guderska@renault.com>
2025-02-03 00:04:08 +0100

    bugs

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-let button = document.getElementById("button");
-let image = document.getElementById("image");
-let pokeNumber = document.getElementById("number");
-let pokeName = document.getElementById("name");
+let button, image, pokeNumber, pokeName;
 
+document.addEventListener('DOMContentLoaded', () => {
+    button = document.getElementById("button");  
+    image = document.getElementById("image");
+    pokeNumber = document.getElementById("number");
+    pokeName = document.getElementById("name"); 
+});
+
+let requestString = `https://pokeapi.co/api/v2/pokemon/${pokeName}/${randomNumber}`;
 
 const changePokemon = async () => {
-    
     let randomNumber = Math.ceil(Math.random() * 150 + 1);
-
-    let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-
+    console.log(randomNumber);
     
-    let data = await fetch(requestString);
+    let data = fetch(requestString);
     console.log(data);
 
-    let response = await data.json();
+    let response = await data.json(string);
     console.log(response);
 
     image.src = response.sprites.front_default;
@@ -23,4 +25,6 @@ const changePokemon = async () => {
 };
 
 changePokemon();
-button.addEventListener("click", changePokemon);
+element.attachEvent("onclick", function() {
+console.log("click");
+    });
